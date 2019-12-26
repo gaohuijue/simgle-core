ebefec2899a0d4b49a796fb20d17e375778abc79
gaohuijue <gaohuijue@shinow.com>
2019-12-26 11:15:30 +0800

    .

--- src/main/kotlin/com/simgle/core/Constant.kt
+++ src/main/kotlin/com/simgle/core/Constant.kt
@@ -2,6 +2,7 @@ package com.simgle.core
 
 object Constant {
     const val RBAC_SESSION = "RBAC_SESSION"
+    const val RBAC_ROOT_USER_ID = "root"
 
     const val DATA_SOURCE_KEY = "DATA_SOURCE_KEY"
 
